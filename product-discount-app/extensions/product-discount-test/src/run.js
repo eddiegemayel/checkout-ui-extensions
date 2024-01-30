7ab03f8b80b3403badc30dcbfbb1b419d74f970f
eddiegemayel <eddiegem16@gmail.com>
2024-01-30 15:58:52 -0500

    Query created. Run.js modified, deployment errors and typescript errors resolved for now. .toml file updated.

--- product-discount-app/extensions/product-discount-test/src/run.js
+++ product-discount-app/extensions/product-discount-test/src/run.js
@@ -1,27 +1,38 @@
 // @ts-check
+// import type { RunInput, FunctionRunResult } from "../generated/api";
 import { DiscountApplicationStrategy } from "../generated/api";
 
-/**
- * @typedef {import("../generated/api").RunInput} RunInput
- * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
- */
-
-/**
- * @type {FunctionRunResult}
- */
 const EMPTY_DISCOUNT = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
   discounts: [],
 };
 
-/**
- * @param {RunInput} input
- * @returns {FunctionRunResult}
- */
 export function run(input) {
-  const configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
-  );
+  const targets = input.cart.lines.filter(line => {
+    if(line.merchandise.__typename == "ProductVariant") {
+      const hasLimitedEditionTag = line.merchandise.product.hasAnyTag;
+      return hasLimitedEditionTag === false;
+    }
+  }).map(line => {
+    return {
+      productVariant: {
+        id: line.merchandise.id,
+      }
+    };
+  });
 
-  return EMPTY_DISCOUNT;
-};
+  const DISCOUNTED_ITEMS = {
+    discountApplicationStrategy: DiscountApplicationStrategy.First,
+    discounts: [{
+      targets: targets,
+      value: {
+        percentage: {
+          value: 10
+        }
+      },
+      message: "10% off"
+    }]
+  }
+
+  return DISCOUNTED_ITEMS;
+}
