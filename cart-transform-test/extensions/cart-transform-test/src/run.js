480b2f8672ccdba7d3025c23fa4915496599df28
eddiegemayel <eddiegem16@gmail.com>
2024-02-19 14:55:48 -0500

    Typegen generated. Run.js updated.

--- cart-transform-test/extensions/cart-transform-test/src/run.js
+++ cart-transform-test/extensions/cart-transform-test/src/run.js
@@ -10,14 +10,39 @@
 /**
  * @type {FunctionRunResult}
  */
-const NO_CHANGES = {
-  operations: [],
-};
 
 /**
  * @param {RunInput} input
  * @returns {FunctionRunResult}
  */
+// Assuming the necessary imports are done in the JavaScript environment as they were in TypeScript
 export function run(input) {
-  return NO_CHANGES;
+  const groupedItems = [];
+  input.cart.lines.forEach(line => {
+    const bundleId = line.bundleId;
+    if(bundleId && bundleId.value) {
+      if(!groupedItems[bundleId.value]) {
+        groupedItems[bundleId.value] = [];
+      }
+      groupedItems[bundleId.value].push(line);
+    }
+  });
+  return {
+    operations: [
+      ...Object.values(groupedItems.map(group => {
+        const mergeOperation = {
+          merge: {
+            cartLines: group.map(line => {
+              return {
+                cartLineId: line.id,
+                quantity: line.quantity
+              }
+            }),
+            parentVariantId: "gid://shopify/ProductVariant/42334054285364"
+          }
+        };
+        return mergeOperation;
+      }))
+    ]
+  };
 };
