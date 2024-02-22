2e624f5569eecd221c48a2664dd8bc0113d2ad2a
eddiegemayel <eddiegem16@gmail.com>
2024-02-22 09:46:34 -0500

    Order admin block created, with 2 graphql API calls. Goes through all products in the order, returns and displays thier tags.

--- admin-order-test/extensions/admin-order-test/src/BlockExtension.jsx
+++ admin-order-test/extensions/admin-order-test/src/BlockExtension.jsx
@@ -4,23 +4,113 @@ import {
   AdminBlock,
   BlockStack,
   Text,
+  Heading,
+  Divider,
 } from '@shopify/ui-extensions-react/admin';
 
-// The target used here must match the target used in the extension's toml file (./shopify.extension.toml)
+import { useState, useEffect } from 'react';
+
 const TARGET = 'admin.order-details.block.render';
 
 export default reactExtension(TARGET, () => <App />);
 
 function App() {
-  // The useApi hook provides access to several useful APIs like i18n and data.
-  const {i18n, data} = useApi(TARGET);
-  console.log({data});
+  //Apis
+  const { data, query } = useApi(TARGET);
+
+  //Current data returned
+  const currentOrder = data.selected[0].id;
+
+  //States
+  const [orderInfo, setOrderInfo] = useState(null);
+  const [lineItem, setLineItem] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await query(`query {
+        order(id: "${ currentOrder }") {
+          lineItems(first: 10) {
+            edges {
+              node {
+                product {
+                  id
+                }
+              }
+            }
+          }
+        }
+      }`);
+
+      if(!result.errors && result.data.order) {
+        if(result.data.order) {
+          result.data.order.lineItems.edges.map((lineItem) => {
+            let productId = lineItem.node.product.id;
+
+            async function fetchProdData() {
+              const productResult = await query(`query {
+                product(id: "${ productId }") {
+                  title
+                  tags
+                }
+              }`);
+
+              //console.log("product query", productResult.data.product);
+              setLineItem(lineItem => [...lineItem, productResult.data.product]);
+            }
+
+            fetchProdData();
+          });
+        }
+
+        setOrderInfo(result.data.order);
+        setError(false);
+      }
+      else {
+        if(result.errors[0]) {
+          setOrderInfo(result.errors[0]);
+        }
+        else {
+          setOrderInfo(result.errors);
+        }
+        setError(true);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     // The AdminBlock component provides an API for setting the title of the Block extension wrapper.
-    <AdminBlock title="My Block Extension">
+    <AdminBlock title="Additional Order Details">
       <BlockStack>
-        <Text fontWeight="bold">{i18n.translate('welcome', {TARGET})}</Text>
+        {error == true &&
+          <>
+            <Text appearance="warning">Error Recieved:</Text>
+            <Text appearance="warning">"{error == true && orderInfo.message}"</Text>
+          </>
+        }
+        
+        {error == false &&
+          <>
+            <Heading size={4}>Product Tags</Heading>
+            {lineItem.map((product) => {
+              // const tagPresent = product.tags.filter((tag) => tag == "Winter");
+
+              return (
+                <BlockStack gap="small base" padding="base base large">
+                  <Text>{product.title}</Text>
+                  <Divider/>
+                    {product.tags.map((tag) => {
+                      return (
+                        <Text>{tag}</Text>
+                      )
+                    })}
+                </BlockStack>
+              )
+            })}
+          </>
+        }
       </BlockStack>
     </AdminBlock>
   );
