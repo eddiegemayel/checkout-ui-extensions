2cfe4770efa12e0c3f4fabd0245485022743b458
eddiegemayel <eddiegem16@gmail.com>
2024-03-15 14:48:38 -0400

    Create Typescript from tutorial. Is not working at all

--- bundle-transform/extensions/cart-transformer/src/run.ts
+++ bundle-transform/extensions/cart-transformer/src/run.ts
@@ -1,14 +1,39 @@
 import type {
   RunInput,
   FunctionRunResult,
+  CartLine,
+  CartOperation
 } from "../generated/api";
 
-const NO_CHANGES: FunctionRunResult = {
-  operations: [],
-};
-
-// gid://shopify/ProductVariant/42334054285364
-
 export function run(input: RunInput): FunctionRunResult {
-  return NO_CHANGES;
+  const groupedItems: Record<string, Pick<CartLine, "id" | "quantity">[]> = {};
+
+  input.cart.lines.forEach((line) => {
+    const bundleId = line.bundleId;
+    if(bundleId && bundleId.value) {
+      if(!groupedItems[bundleId.value]) {
+        groupedItems[bundleId.value] = [];
+      }
+      groupedItems[bundleId.value].push(line);
+    }
+  });
+
+  return {
+    operations: [
+      ...Object.values(groupedItems.map((group) => {
+        const mergeOperation: CartOperation = {
+          merge: {
+            cartLines: group.map((line) => {
+              return {
+                cartLineId: line.id,
+                quantity: line.quantity,
+              };
+            }),
+            parentVariantId: "gid://shopify/ProductVariant/42334054285364",
+          },
+        };
+        return mergeOperation;
+      })),
+    ],
+  };
 };
